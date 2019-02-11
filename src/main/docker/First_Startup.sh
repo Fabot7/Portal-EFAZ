87c07c323be61001a2b16d274055ec123a7377e4
Isadora <isadoranezes@gmail.com>
2019-02-11 15:11:29 -0300

    Tutorial Tomcat

--- src/main/docker/First_Startup.sh
+++ src/main/docker/First_Startup.sh
@@ -3,4 +3,4 @@
 mkdir -p /var/lib/tomcat8/webapps/ROOT
 cp -r /var/tmp/OPENCMS/* /var/lib/tomcat8/webapps/ROOT
 
-exec /usr/sbin/apache2 -DFOREGROUND
+source /usr/sbin/envvars && exec /usr/sbin/apache2 -D FOREGROUND
